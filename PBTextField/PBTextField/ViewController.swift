5568b04ad9dfbb7007ea229dea5648530332f2af
pradeep burugu <pradeep.burugu@tispr.com>
2016-11-13 17:22:51 -0800

    clean the code

--- PBTextField/PBTextField/ViewController.swift
+++ PBTextField/PBTextField/ViewController.swift
@@ -10,31 +10,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    @IBOutlet var name: PBTextField!
     var label: PBTextField = PBTextField(frame: CGRect(x: 25, y: 225, width: 100, height: 25))
     override func viewDidLoad() {
         super.viewDidLoad()
-        print("viewdidload:\(name.frame.size.width)")
-        name.normalLabelText = "asdas"
-        let rview = UIButton()
-        rview.frame.size = CGSize(width: 24, height: 24)
-        rview.setBackgroundImage(UIImage(named: "hidden"), for: .normal)
-        rview.addTarget(self, action: #selector(ViewController.toggle), for: .touchUpInside)
-        label.rightView = rview
-        label.rightViewMode = .whileEditing
-        // Do any additional setup after loading the view, typically from a nib.
-    //    name.errorLabelText = "Hello"
-        //label.clearButtonMode = .whileEditing
-        label.isSecureTextEntry = true
+
         label.errorLabelText = "Halo"
         view.addSubview(label)
-        
-
-        
-    }
-    
-    func toggle() {
-        label.isSecureTextEntry = !label.isSecureTextEntry
     }
 
     override func didReceiveMemoryWarning() {
